ee33db0bbc7e201bd405365418942dfbb713b4be
tomas <tomas@DESKTOP-OLN67J4>
2021-10-28 18:53:48 +0100

    Maven Commit

--- ADSTestProject/src/FirstClass.java
+++ ADSTestProject/src/FirstClass.java
@@ -2,8 +2,7 @@
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("AAAAAAAAAAAAAA");
-		System.out.println("ADS - ADS");
+		System.out.println("AAAAAAAAAAAAAAAAAA");
 
 	}
 
