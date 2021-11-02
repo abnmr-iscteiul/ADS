fd2893209ae60ba16054f39040af8a3b01d0306b
tomas <tomas@DESKTOP-OLN67J4>
2021-11-02 11:33:05 +0000

    @CsvBindByPosition adicionado

--- ADSTestProject/src/CsvImporter.java
+++ ADSTestProject/src/CsvImporter.java
@@ -1,24 +1,46 @@
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class CsvImporter {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, CsvException {
 
-        String fileName = "/Users/chainz/Desktop/ADS - Caracterizacao das salas.csv";
-
-        List<Sala> beans = new CsvToBeanBuilder(new FileReader(fileName))
+        //String fileName = "/Users/chainz/Desktop/ADS - Caracterizacao das salas.csv";
+    	String fileName = "C:/ADS-Caracterizacao Salas.csv";
+    	/*
+    	//CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build(); // custom separator
+    	@SuppressWarnings({ "unchecked", "rawtypes" })
+		List<Sala> beans = new CsvToBeanBuilder(new FileReader(fileName))
                 .withType(Sala.class)
                 .build()
                 .parse();
 
-        beans.forEach(System.out::println);
-        System.out.println("Hello World");
+        beans.forEach(x -> System.out.println(x));
+        */
 
-
+        
+        
+        
+        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build(); // custom separator
+        try(CSVReader reader = new CSVReaderBuilder(
+                new FileReader(fileName))
+                .withCSVParser(csvParser)   // custom CSV parser
+                //.withSkipLines(1)           // skip the first line, header info
+                .build()){
+            List<String[]> r = reader.readAll();
+            r.forEach(x -> System.out.println(Arrays.toString(x)));
+        }
     }
 
 }
--- ADSTestProject/src/Sala.java
+++ ADSTestProject/src/Sala.java
@@ -1,40 +1,83 @@
+import com.opencsv.bean.CsvBindByPosition;
 
 public class Sala {
 
+	@CsvBindByPosition(position = 0)
 	String edificio;
+	
+	@CsvBindByPosition(position = 1)
 	String nome;
+	
+	@CsvBindByPosition(position = 2)
 	int capacidadeNormal;
+	
+	@CsvBindByPosition(position = 3)
 	int capacidadeExame;
+	
+	@CsvBindByPosition(position = 4)
 	int nCaracteristicas;
+	
+	@CsvBindByPosition(position = 5)
 	boolean anfiteatroAulas;
+	
+	@CsvBindByPosition(position = 6)
 	boolean apoioTecnicoEventos;
+	
+	@CsvBindByPosition(position = 7)
 	boolean Arq1;
+	@CsvBindByPosition(position = 8)
 	boolean Arq2;
+	@CsvBindByPosition(position = 9)
 	boolean Arq3;
+	@CsvBindByPosition(position = 10)
 	boolean Arq4;
+	@CsvBindByPosition(position = 11)
 	boolean Arq5;
+	@CsvBindByPosition(position = 12)
 	boolean Arq6;
+	@CsvBindByPosition(position = 13)
 	boolean Arq9;
+	@CsvBindByPosition(position = 14)
 	boolean BYOD;
+	@CsvBindByPosition(position = 15)
 	boolean focusGroup;
+	@CsvBindByPosition(position = 16)
 	boolean horarioSalaVisivelPortalPublico;
+	@CsvBindByPosition(position = 17)
 	boolean laboratorioDeArquitecturaDeComputadoresI;
+	@CsvBindByPosition(position = 18)
 	boolean laboratorioDeArquitecturaDeComputadoresII;
+	@CsvBindByPosition(position = 19)
 	boolean laboratorioDeBasesDeEngenharia;
+	@CsvBindByPosition(position = 20)
 	boolean laboratorioDeElectronica;
+	@CsvBindByPosition(position = 21)
 	boolean laboratorioDeInformatica;
+	@CsvBindByPosition(position = 22)
 	boolean laboratorioDeJornalismo;
+	@CsvBindByPosition(position = 23)
 	boolean laboratorioDeRedesDeComputadoresI;
+	@CsvBindByPosition(position = 24)
 	boolean laboratorioDeRedesDeComputadoresII;
+	@CsvBindByPosition(position = 25)
 	boolean laboratorioDeTelecomunicacoes;
+	@CsvBindByPosition(position = 26)
 	boolean salaAulasMestrado;
+	@CsvBindByPosition(position = 27)
 	boolean salaAulasMestradoPlus;
+	@CsvBindByPosition(position = 28)
 	boolean salaNEE;
+	@CsvBindByPosition(position = 29)
 	boolean salaProvas;
+	@CsvBindByPosition(position = 30)
 	boolean salaReuniao;
+	@CsvBindByPosition(position = 31)
 	boolean salaDeArquitectura;
+	@CsvBindByPosition(position = 32)
 	boolean salaDeAulasNormal;
+	@CsvBindByPosition(position = 33)
 	boolean videoconferencia;
+	@CsvBindByPosition(position = 34)
 	boolean atrio;
 
 }
