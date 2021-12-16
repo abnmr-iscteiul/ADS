b33ca01a32c932f20cc16f13524a5dc6c1abf471
danibras <danifjbras11@gmail.com>
2021-12-16 23:44:50 +0000

    Download button now working

--- demo/src/main/java/com/example/demo/controller.java
+++ demo/src/main/java/com/example/demo/controller.java
@@ -8,9 +8,13 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,6 +44,22 @@ public class controller {
 		model.addAttribute("nome","FIFO");		
 		return "res";
 	}
+
+	@GetMapping("/download/{algoritmo}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable String algoritmo) {
+		Path path =Paths.get("src/main/resources", "final"+algoritmo+".csv");
+        ByteArrayResource resource=null;
+        try {
+            resource = new ByteArrayResource(Files.readAllBytes(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok()
+                .header(algoritmo)
+                .contentType(MediaType.parseMediaType("text/csv"))
+                .body(resource);
+    }
 	
 	@GetMapping("/LIFO")
 	public String lifo(Model model) {		
@@ -68,7 +88,7 @@ public class controller {
 		model.addAttribute("nome","Aleatório");		
 		return "res";
 	}
-	@GetMapping("/LCF")
+	@GetMapping("/LOWERCAPACITYFIRST")
 	public String lcf(Model model) {		
 		String csvOutputFile;
 		try {
@@ -78,7 +98,7 @@ public class controller {
 			e.printStackTrace();
 		}
 		model.addAttribute("resultado",resultado);
-		model.addAttribute("nome","Menor capacidade primeiro");		
+		model.addAttribute("nome","Menor Capacidade Primeiro");		
 		return "res";
 	}	
 	@PostMapping("/post")
@@ -95,7 +115,7 @@ public class controller {
 		
 		System.out.println(caracter);
 		
-		if(caracter!=null)
+		if(caracter.equals("true"))
 			caract=true;
 		else
 			caract=false;
